9a2d20af1053e94df0f2ba98cc8d02c4530dee3b
joachim929 <joachim_urquhart@hotmail.co.uk>
2019-01-03 22:20:12 +0100

    Refactored database connection for menu

--- menu/DeleteMenu.php
+++ menu/DeleteMenu.php
@@ -1,7 +1,24 @@
 <?php
 require_once '../ConnectDb.php';
 
-class DeleteMenu extends ConnectDb
+class DeleteMenu
 {
+    /**
+     * @var ConnectDb|null
+     */
+    private $conn;
 
+    /**
+     * @var mysqli
+     */
+    private $mysqli;
+
+    private $connectDb;
+
+    function __construct()
+    {
+        $this->connectDb = new ConnectDb();
+        $this->conn = $this->connectDb->getInstance();
+        $this->mysqli = $this->conn->getConnection();
+    }
 }
