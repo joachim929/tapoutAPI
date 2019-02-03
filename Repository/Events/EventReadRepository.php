14e22e01eb5b27c55b343e9e7cd3df3ece6cf8ed
joachim929 <joachim_urquhart@hotmail.co.uk>
2019-02-04 00:44:30 +0100

    Created get event category and item by id

--- Repository/Events/EventReadRepository.php
+++ Repository/Events/EventReadRepository.php
@@ -31,6 +31,98 @@ class EventReadRepository
         $this->mysqli = $this->conn->getConnection();
     }
 
+    /**
+     * This function gets an event item with a given provided itemId, if one isn't found false is returned
+     * @param int $itemId
+     * @return AdminEventItem|false
+     */
+    public function getEventItem(int $itemId)
+    {
+        $stmt = $this->mysqli->prepare(
+            'SELECT item.category_id AS catId, item.id AS itemId, item.event_start AS start, item.event_end AS end,
+            item.category_position AS catPosition, item.uses_start_time AS usesStartTime, 
+            item.uses_end_time AS usesEndTime, item.uses_end_date AS usesEndDate, item.created_at AS itemCreatedAt,
+            item.edited_at AS itemEditedAt, item.active AS itemActive,
+            enDetails.id AS enId, enDetails.title AS enTitle, enDetails.description AS enDescripton, 
+            enDetails.created_at AS enCreatedAt, enDetails.edited_at AS enEditedAt,
+            vnDetails.id AS vnId, vnDetails.title AS vnTitle, vnDetails.description AS vnDescripton, 
+            vnDetails.created_at AS vnCreatedAt, vnDetails.edited_at AS vnEditedAt
+            FROM event_item AS item
+            LEFT JOIN event_item_details AS enDetails ON item.id = enDetails.item_id AND enDetails.language = "en"
+            LEFT JOIN event_item_details AS vnDetails ON item.id = vnDetails.item_id AND vnDetails.language = "vn"
+            WHERE item.id = ?'
+        );
+
+        $stmt->bind_param('i', $itemId);
+
+        $stmt->execute();
+
+        $stmt->bind_result($catId, $itemId, $start, $end, $catPosition, $usesStartTime, $usesEndTime, $usesEndDate, $itemCreatedAt,
+            $itemEditedAt, $itemActive, $enId, $enTitle, $enDescription, $enCreatedAt, $enEditedAt, $vnId, $vnTitle,
+            $vnDescription, $vnCreatedAt, $vnEditedAt);
+
+        $stmt->fetch();
+
+        $result = new AdminEventItem(
+            $catId, $catPosition, $itemId, new DateTime($itemCreatedAt), new DateTime($itemEditedAt),
+            $enId, $enTitle, $enDescription, new DateTime($enCreatedAt), new DateTime($enEditedAt), $vnId,
+            $vnTitle, $vnDescription, new DateTime($vnCreatedAt), new DateTime($vnEditedAt), new DateTime($start),
+            new DateTime($end), $usesStartTime, $usesEndTime, $usesEndDate, $itemActive
+        );
+
+        if ($stmt->errno) {
+            $result = false;
+        }
+
+        $stmt->close();
+
+        return $result;
+    }
+
+    /**
+     * This function gets an event category with a given provided categoryId, if one isn't found false is returned
+     * @param int $categoryId
+     * @return AdminEventCategory|false
+     */
+    public function getEventCategory(int $categoryId)
+    {
+
+        $stmt = $this->mysqli->prepare(
+            'SELECT cat.id AS catId, cat.en_name AS catEnName, cat.vn_name AS catVnName,
+            cat.type AS catType, cat.page_position AS pagePosition, cat.active AS catActive, cat.created_at AS catCreatedAt, 
+            cat.edited_at AS catEditedAt
+            FROM event_category AS cat
+            WHERE cat.id = ?'
+        );
+        $stmt->bind_param('i', $categoryId);
+
+        $stmt->execute();
+
+        $stmt->bind_result($catId, $catEnName, $catVnName, $catType, $pagePosition, $catActive, $catCreatedAt,
+            $catEditedAt);
+
+        $stmt->fetch();
+
+        $result = new AdminEventCategory(
+            $catId, $catEnName, $catVnName, $catType, $pagePosition,
+            new DateTime($catCreatedAt), new DateTime($catEditedAt), $catActive
+        );
+
+
+
+        if ($stmt->errno) {
+            $result = false;
+        }
+
+        $stmt->close();
+
+        return $result;
+    }
+
+    /**
+     * This function gets all event categories and items, if none are found it returns false
+     * @return array|false
+     */
     public function getAllEvents()
     {
         $results = [];
@@ -86,6 +178,12 @@ class EventReadRepository
         return $results;
     }
 
+    /**
+     * This function gets all event categories and items with a given language where the category and items are active
+     * If none are found false will be returned
+     * @param $language
+     * @return array|false
+     */
     public function getEventsByLanguage($language)
     {
         $results = [];
