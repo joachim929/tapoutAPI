0c4795010073297d5ef552731f53f733236d7807
joachim929 <joachim_urquhart@hotmail.co.uk>
2019-02-03 22:42:23 +0100

    Made changes to events tables

--- tap_out.sql
+++ tap_out.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2019 at 08:03 PM
+-- Generation Time: Feb 03, 2019 at 10:40 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
@@ -44,14 +44,14 @@ CREATE TABLE `event_category` (
 --
 
 INSERT INTO `event_category` (`id`, `en_name`, `vn_name`, `type`, `active`, `page_position`, `created_at`, `edited_at`) VALUES
-(1, 'ENWeekly', '', 'weekly', 1, 1, '2018-10-14 08:44:30', NULL),
-(2, 'VNUnique', '', 'unique', 1, 4, '2018-10-14 08:53:03', NULL),
-(3, 'ENUnique', '', 'unique', 1, 4, '2018-10-14 08:53:03', NULL),
-(5, 'ENData', '', 'unique', 1, 2, '2018-10-14 08:53:03', NULL),
-(6, 'VNData', '', 'unique', 1, 2, '2018-10-14 08:53:03', NULL),
-(10, 'VNWeekly', '', 'weekly', 1, 1, '2018-10-14 08:44:30', NULL),
-(11, 'VNTest Active', '', 'weekly', 0, 1, '2018-10-14 08:44:30', NULL),
-(12, 'ENTest Active', '', 'weekly', 0, 99, '2018-10-14 08:44:30', NULL);
+(1, 'ENWeekly 1', 'VNWeekly 1', 'weekly', 1, 1, '2018-10-14 08:44:30', NULL),
+(2, 'ENUnique 1', 'VNUnique 1', 'unique', 1, 6, '2018-10-14 08:53:03', NULL),
+(3, 'ENTEST Unique 2', 'VNTEST Unique 2', 'unique', 1, 7, '2018-10-14 08:53:03', NULL),
+(4, 'EN Weekly 2', 'VN Weekly 2', 'weekly', 1, 2, '2018-10-14 08:44:30', NULL),
+(5, 'ENTEST Unique 3', 'VNTEST Unique 3', 'unique', 1, 4, '2018-10-14 08:53:03', NULL),
+(6, 'ENTEST Unique 4', 'VNTEST Unique 4', 'unique', 1, 5, '2018-10-14 08:53:03', NULL),
+(7, 'EN Weekly 3', 'VN Weekly 3', 'weekly', 1, 3, '2018-10-14 08:44:30', NULL),
+(8, 'EN Weekly 4', 'VN Weekly 4', 'weekly', 1, 8, '2018-10-14 08:44:30', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,32 +62,74 @@ INSERT INTO `event_category` (`id`, `en_name`, `vn_name`, `type`, `active`, `pag
 CREATE TABLE `event_item` (
   `id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `start_time` time DEFAULT NULL,
-  `end_time` time DEFAULT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date DEFAULT NULL,
+  `event_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `event_end` timestamp NULL DEFAULT NULL,
   `category_position` int(11) NOT NULL,
+  `uses_start_time` tinyint(4) NOT NULL,
+  `uses_end_time` tinyint(4) NOT NULL,
+  `uses_end_date` tinyint(4) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edited_at` timestamp NULL DEFAULT NULL
+  `edited_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `event_item`
 --
 
-INSERT INTO `event_item` (`id`, `category_id`, `start_time`, `end_time`, `start_date`, `end_date`, `category_position`, `created_at`, `edited_at`) VALUES
-(1, 1, '16:00:00', '19:00:00', '0000-00-00', NULL, 0, '0000-00-00 00:00:00', NULL),
-(2, 1, '16:00:00', '20:00:00', '0000-00-00', NULL, 2, '0000-00-00 00:00:00', NULL),
-(3, 1, '17:00:00', '21:00:00', '0000-00-00', NULL, 1, '0000-00-00 00:00:00', NULL),
-(4, 1, '18:00:00', '22:00:00', '0000-00-00', NULL, 4, '0000-00-00 00:00:00', NULL),
-(5, 1, '19:00:00', '23:00:00', '0000-00-00', NULL, 3, '0000-00-00 00:00:00', NULL),
-(6, 3, '20:00:00', '00:00:00', '0000-00-00', NULL, 0, '0000-00-00 00:00:00', NULL),
-(7, 10, '16:00:00', '19:00:00', '0000-00-00', NULL, 0, '0000-00-00 00:00:00', NULL),
-(8, 10, '16:00:00', '20:00:00', '0000-00-00', NULL, 2, '0000-00-00 00:00:00', NULL),
-(9, 10, '17:00:00', '21:00:00', '0000-00-00', NULL, 1, '0000-00-00 00:00:00', NULL),
-(10, 10, '18:00:00', '22:00:00', '0000-00-00', NULL, 4, '0000-00-00 00:00:00', NULL),
-(11, 10, '19:00:00', '23:00:00', '0000-00-00', NULL, 3, '0000-00-00 00:00:00', NULL),
-(12, 2, '20:00:00', '00:00:00', '0000-00-00', NULL, 0, '0000-00-00 00:00:00', NULL);
+INSERT INTO `event_item` (`id`, `category_id`, `event_start`, `event_end`, `category_position`, `uses_start_time`, `uses_end_time`, `uses_end_date`, `created_at`, `edited_at`) VALUES
+(1, 1, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 1, 0, 0, 1, '0000-00-00 00:00:00', '2019-02-03 21:24:11'),
+(2, 1, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 21:24:42'),
+(3, 2, '2019-02-14 23:00:00', '2020-02-03 19:46:10', 1, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 21:06:30'),
+(4, 2, '2019-02-14 23:00:00', '2020-02-03 19:46:10', 4, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 21:06:41'),
+(5, 3, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:45:23'),
+(6, 3, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 0, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:45:26'),
+(7, 4, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 0, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:45:29'),
+(8, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 2, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:45:31'),
+(9, 6, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 1, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:44:53'),
+(10, 7, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:44:45'),
+(11, 8, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:44:31'),
+(12, 8, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 0, 0, 0, 0, '0000-00-00 00:00:00', '2019-02-03 20:44:27'),
+(16, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:13:39', NULL),
+(17, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:14:13', NULL),
+(18, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 10, 0, 0, 0, '2019-02-03 16:14:13', '2019-02-03 21:24:28'),
+(19, 8, '2019-02-17 23:00:00', '2019-02-03 19:46:10', 8, 0, 0, 0, '2019-02-03 16:14:13', NULL),
+(20, 8, '2019-02-17 23:00:00', '2019-02-03 19:46:10', 8, 0, 0, 0, '2019-02-03 16:14:14', NULL),
+(21, 7, '2019-02-16 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '2019-02-03 16:14:14', NULL),
+(22, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '2019-02-03 16:14:14', '2019-02-03 21:24:26'),
+(23, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:14:14', '2019-02-03 21:24:23'),
+(24, 7, '2019-02-16 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '2019-02-03 16:14:15', NULL),
+(25, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '2019-02-03 16:14:15', '2019-02-03 21:24:19'),
+(26, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:14:15', '2019-02-03 21:24:15'),
+(27, 3, '2019-02-12 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '2019-02-03 16:14:58', NULL),
+(28, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:14:59', '2019-02-03 21:06:23'),
+(29, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:14:59', NULL),
+(30, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:14:59', '2019-02-03 21:07:18'),
+(31, 6, '2019-02-15 23:00:00', '2019-02-03 19:46:10', 6, 0, 0, 0, '2019-02-03 16:14:59', NULL),
+(32, 8, '2019-02-17 23:00:00', '2019-02-03 19:46:10', 8, 0, 0, 0, '2019-02-03 16:14:59', NULL),
+(33, 4, '2019-02-13 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '2019-02-03 16:14:59', NULL),
+(34, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 8, 0, 0, 0, '2019-02-03 16:14:59', '2019-02-03 21:24:38'),
+(35, 3, '2019-02-12 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '2019-02-03 16:14:59', NULL),
+(36, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:14:59', '2019-02-03 21:06:59'),
+(37, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:15:00', '2019-02-03 21:06:36'),
+(38, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(39, 4, '2019-02-13 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(40, 6, '2019-02-15 23:00:00', '2019-02-03 19:46:10', 6, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(41, 4, '2019-02-13 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(42, 6, '2019-02-15 23:00:00', '2019-02-03 19:46:10', 6, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(43, 3, '2019-02-12 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(44, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:15:00', '2019-02-03 21:07:13'),
+(45, 7, '2019-02-16 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '2019-02-03 16:15:00', NULL),
+(46, 4, '2019-02-13 23:00:00', '2019-02-03 19:46:10', 4, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(47, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 9, 0, 0, 0, '2019-02-03 16:15:01', '2019-02-03 21:24:35'),
+(48, 1, '2019-02-10 23:00:00', '2019-02-03 19:46:10', 6, 0, 0, 0, '2019-02-03 16:15:01', '2019-02-03 21:24:46'),
+(49, 3, '2019-02-12 23:00:00', '2019-02-03 19:46:10', 3, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(50, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(51, 8, '2019-02-17 23:00:00', '2019-02-03 19:46:10', 8, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(52, 7, '2019-02-16 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(53, 2, '2019-02-11 23:00:00', '2020-02-03 19:46:10', 2, 0, 0, 0, '2019-02-03 16:15:01', '2019-02-03 21:06:18'),
+(54, 6, '2019-02-15 23:00:00', '2019-02-03 19:46:10', 6, 0, 0, 0, '2019-02-03 16:15:01', NULL),
+(55, 5, '2019-02-14 23:00:00', '2019-02-03 19:46:10', 5, 0, 0, 0, '2019-02-03 16:15:02', NULL),
+(56, 7, '2019-02-16 23:00:00', '2019-02-03 19:46:10', 7, 0, 0, 0, '2019-02-03 16:15:02', NULL);
 
 -- --------------------------------------------------------
 
@@ -104,6 +146,118 @@ CREATE TABLE `event_item_details` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `edited_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `event_item_details`
+--
+
+INSERT INTO `event_item_details` (`id`, `item_id`, `title`, `description`, `language`, `created_at`, `edited_at`) VALUES
+(1, 1, 'test en 1', 'test en 1', 'en', '2019-02-03 15:45:00', NULL),
+(2, 1, 'test vn 1', 'test vn 1', 'vn', '2019-02-03 15:45:00', NULL),
+(3, 2, 'test en 2', 'test en 2', 'en', '2019-02-03 15:45:30', NULL),
+(4, 2, 'test vn 2', 'test vn 2', 'vn', '2019-02-03 15:45:30', NULL),
+(5, 3, 'test en 3', 'test en 3', 'en', '2019-02-03 15:45:54', NULL),
+(6, 3, 'test vn 3', 'test vn 3', 'vn', '2019-02-03 15:45:54', NULL),
+(7, 4, 'test en 4', 'test en 4', 'en', '2019-02-03 15:46:24', NULL),
+(8, 4, 'test vn 4', 'test vn 4', 'vn', '2019-02-03 15:46:24', NULL),
+(9, 5, 'test en 5', 'test en 5', 'en', '2019-02-03 15:46:43', NULL),
+(10, 5, 'test vn 5', 'test vn 5', 'vn', '2019-02-03 15:46:43', NULL),
+(11, 6, 'test en 6', 'test en 6', 'en', '2019-02-03 15:47:03', NULL),
+(12, 6, 'test vn 6', 'test vn 6', 'vn', '2019-02-03 15:47:03', NULL),
+(13, 7, 'test en 7', 'test en 7', 'en', '2019-02-03 15:47:22', NULL),
+(14, 7, 'test vn 7', 'test vn 7', 'vn', '2019-02-03 15:47:22', NULL),
+(15, 8, 'test en 8', 'test en 8', 'en', '2019-02-03 15:47:56', NULL),
+(16, 8, 'test vn 8', 'test vn 7', 'vn', '2019-02-03 15:47:56', NULL),
+(17, 9, 'test en 9', 'test en 9', 'en', '2019-02-03 15:48:35', NULL),
+(18, 9, 'test vn 9', 'test vn 9', 'vn', '2019-02-03 15:48:35', NULL),
+(19, 10, 'test en 10', 'test en 10', 'en', '2019-02-03 15:48:50', NULL),
+(20, 10, 'test vn 10', 'test vn 10', 'vn', '2019-02-03 15:48:50', NULL),
+(21, 11, 'test en 11', 'test en 11', 'en', '2019-02-03 15:49:05', NULL),
+(22, 11, 'test vn 11', 'test vn 11', 'vn', '2019-02-03 15:49:05', NULL),
+(23, 12, 'test en 12', 'test en 12', 'en', '2019-02-03 15:49:18', NULL),
+(24, 12, 'test vn 12', 'test vn 12', 'vn', '2019-02-03 15:49:18', NULL),
+(29, 16, 'En Test 16', 'En Description Test 16', 'en', '2019-02-03 16:13:39', NULL),
+(30, 16, 'Vn Test 16', 'Vn Description Test 16', 'vn', '2019-02-03 16:13:39', NULL),
+(31, 17, 'En Test 17', 'En Description Test 17', 'en', '2019-02-03 16:14:13', NULL),
+(32, 17, 'Vn Test 17', 'Vn Description Test 17', 'vn', '2019-02-03 16:14:13', NULL),
+(33, 18, 'En Test 18', 'En Description Test 18', 'en', '2019-02-03 16:14:13', NULL),
+(34, 18, 'Vn Test 18', 'Vn Description Test 18', 'vn', '2019-02-03 16:14:13', NULL),
+(35, 19, 'En Test 19', 'En Description Test 19', 'en', '2019-02-03 16:14:14', NULL),
+(36, 19, 'Vn Test 19', 'Vn Description Test 19', 'vn', '2019-02-03 16:14:14', NULL),
+(37, 20, 'En Test 20', 'En Description Test 20', 'en', '2019-02-03 16:14:14', NULL),
+(38, 20, 'Vn Test 20', 'Vn Description Test 20', 'vn', '2019-02-03 16:14:14', NULL),
+(39, 21, 'En Test 21', 'En Description Test 21', 'en', '2019-02-03 16:14:14', NULL),
+(40, 21, 'Vn Test 21', 'Vn Description Test 21', 'vn', '2019-02-03 16:14:14', NULL),
+(41, 22, 'En Test 22', 'En Description Test 22', 'en', '2019-02-03 16:14:14', NULL),
+(42, 22, 'Vn Test 22', 'Vn Description Test 22', 'vn', '2019-02-03 16:14:14', NULL),
+(43, 23, 'En Test 23', 'En Description Test 23', 'en', '2019-02-03 16:14:14', NULL),
+(44, 23, 'Vn Test 23', 'Vn Description Test 23', 'vn', '2019-02-03 16:14:14', NULL),
+(45, 24, 'En Test 24', 'En Description Test 24', 'en', '2019-02-03 16:14:15', NULL),
+(46, 24, 'Vn Test 24', 'Vn Description Test 24', 'vn', '2019-02-03 16:14:15', NULL),
+(47, 25, 'En Test 25', 'En Description Test 25', 'en', '2019-02-03 16:14:15', NULL),
+(48, 25, 'Vn Test 25', 'Vn Description Test 25', 'vn', '2019-02-03 16:14:15', NULL),
+(49, 26, 'En Test 26', 'En Description Test 26', 'en', '2019-02-03 16:14:15', NULL),
+(50, 26, 'Vn Test 26', 'Vn Description Test 26', 'vn', '2019-02-03 16:14:15', NULL),
+(51, 27, 'En Test 27', 'En Description Test 27', 'en', '2019-02-03 16:14:58', NULL),
+(52, 27, 'Vn Test 27', 'Vn Description Test 27', 'vn', '2019-02-03 16:14:58', NULL),
+(53, 28, 'En Test 28', 'En Description Test 28', 'en', '2019-02-03 16:14:59', NULL),
+(54, 28, 'Vn Test 28', 'Vn Description Test 28', 'vn', '2019-02-03 16:14:59', NULL),
+(55, 29, 'En Test 29', 'En Description Test 29', 'en', '2019-02-03 16:14:59', NULL),
+(56, 29, 'Vn Test 29', 'Vn Description Test 29', 'vn', '2019-02-03 16:14:59', NULL),
+(57, 30, 'En Test 30', 'En Description Test 30', 'en', '2019-02-03 16:14:59', NULL),
+(58, 30, 'Vn Test 30', 'Vn Description Test 30', 'vn', '2019-02-03 16:14:59', NULL),
+(59, 31, 'En Test 31', 'En Description Test 31', 'en', '2019-02-03 16:14:59', NULL),
+(60, 31, 'Vn Test 31', 'Vn Description Test 31', 'vn', '2019-02-03 16:14:59', NULL),
+(61, 32, 'En Test 32', 'En Description Test 32', 'en', '2019-02-03 16:14:59', NULL),
+(62, 32, 'Vn Test 32', 'Vn Description Test 32', 'vn', '2019-02-03 16:14:59', NULL),
+(63, 33, 'En Test 33', 'En Description Test 33', 'en', '2019-02-03 16:14:59', NULL),
+(64, 33, 'Vn Test 33', 'Vn Description Test 33', 'vn', '2019-02-03 16:14:59', NULL),
+(65, 34, 'En Test 34', 'En Description Test 34', 'en', '2019-02-03 16:14:59', NULL),
+(66, 34, 'Vn Test 34', 'Vn Description Test 34', 'vn', '2019-02-03 16:14:59', NULL),
+(67, 35, 'En Test 35', 'En Description Test 35', 'en', '2019-02-03 16:14:59', NULL),
+(68, 35, 'Vn Test 35', 'Vn Description Test 35', 'vn', '2019-02-03 16:14:59', NULL),
+(69, 36, 'En Test 36', 'En Description Test 36', 'en', '2019-02-03 16:15:00', NULL),
+(70, 36, 'Vn Test 36', 'Vn Description Test 36', 'vn', '2019-02-03 16:15:00', NULL),
+(71, 37, 'En Test 37', 'En Description Test 37', 'en', '2019-02-03 16:15:00', NULL),
+(72, 37, 'Vn Test 37', 'Vn Description Test 37', 'vn', '2019-02-03 16:15:00', NULL),
+(73, 38, 'En Test 38', 'En Description Test 38', 'en', '2019-02-03 16:15:00', NULL),
+(74, 38, 'Vn Test 38', 'Vn Description Test 38', 'vn', '2019-02-03 16:15:00', NULL),
+(75, 39, 'En Test 39', 'En Description Test 39', 'en', '2019-02-03 16:15:00', NULL),
+(76, 39, 'Vn Test 39', 'Vn Description Test 39', 'vn', '2019-02-03 16:15:00', NULL),
+(77, 40, 'En Test 40', 'En Description Test 40', 'en', '2019-02-03 16:15:00', NULL),
+(78, 40, 'Vn Test 40', 'Vn Description Test 40', 'vn', '2019-02-03 16:15:00', NULL),
+(79, 41, 'En Test 41', 'En Description Test 41', 'en', '2019-02-03 16:15:00', NULL),
+(80, 41, 'Vn Test 41', 'Vn Description Test 41', 'vn', '2019-02-03 16:15:00', NULL),
+(81, 42, 'En Test 42', 'En Description Test 42', 'en', '2019-02-03 16:15:00', NULL),
+(82, 42, 'Vn Test 42', 'Vn Description Test 42', 'vn', '2019-02-03 16:15:00', NULL),
+(83, 43, 'En Test 43', 'En Description Test 43', 'en', '2019-02-03 16:15:00', NULL),
+(84, 43, 'Vn Test 43', 'Vn Description Test 43', 'vn', '2019-02-03 16:15:00', NULL),
+(85, 44, 'En Test 44', 'En Description Test 44', 'en', '2019-02-03 16:15:00', NULL),
+(86, 44, 'Vn Test 44', 'Vn Description Test 44', 'vn', '2019-02-03 16:15:00', NULL),
+(87, 45, 'En Test 45', 'En Description Test 45', 'en', '2019-02-03 16:15:00', NULL),
+(88, 45, 'Vn Test 45', 'Vn Description Test 45', 'vn', '2019-02-03 16:15:00', NULL),
+(89, 46, 'En Test 46', 'En Description Test 46', 'en', '2019-02-03 16:15:01', NULL),
+(90, 46, 'Vn Test 46', 'Vn Description Test 46', 'vn', '2019-02-03 16:15:01', NULL),
+(91, 47, 'En Test 47', 'En Description Test 47', 'en', '2019-02-03 16:15:01', NULL),
+(92, 47, 'Vn Test 47', 'Vn Description Test 47', 'vn', '2019-02-03 16:15:01', NULL),
+(93, 48, 'En Test 48', 'En Description Test 48', 'en', '2019-02-03 16:15:01', NULL),
+(94, 48, 'Vn Test 48', 'Vn Description Test 48', 'vn', '2019-02-03 16:15:01', NULL),
+(95, 49, 'En Test 49', 'En Description Test 49', 'en', '2019-02-03 16:15:01', NULL),
+(96, 49, 'Vn Test 49', 'Vn Description Test 49', 'vn', '2019-02-03 16:15:01', NULL),
+(97, 50, 'En Test 50', 'En Description Test 50', 'en', '2019-02-03 16:15:01', NULL),
+(98, 50, 'Vn Test 50', 'Vn Description Test 50', 'vn', '2019-02-03 16:15:01', NULL),
+(99, 51, 'En Test 51', 'En Description Test 51', 'en', '2019-02-03 16:15:01', NULL),
+(100, 51, 'Vn Test 51', 'Vn Description Test 51', 'vn', '2019-02-03 16:15:01', NULL),
+(101, 52, 'En Test 52', 'En Description Test 52', 'en', '2019-02-03 16:15:01', NULL),
+(102, 52, 'Vn Test 52', 'Vn Description Test 52', 'vn', '2019-02-03 16:15:01', NULL),
+(103, 53, 'En Test 53', 'En Description Test 53', 'en', '2019-02-03 16:15:01', NULL),
+(104, 53, 'Vn Test 53', 'Vn Description Test 53', 'vn', '2019-02-03 16:15:01', NULL),
+(105, 54, 'En Test 54', 'En Description Test 54', 'en', '2019-02-03 16:15:02', NULL),
+(106, 54, 'Vn Test 54', 'Vn Description Test 54', 'vn', '2019-02-03 16:15:02', NULL),
+(107, 55, 'En Test 55', 'En Description Test 55', 'en', '2019-02-03 16:15:02', NULL),
+(108, 55, 'Vn Test 55', 'Vn Description Test 55', 'vn', '2019-02-03 16:15:02', NULL),
+(109, 56, 'En Test 56', 'En Description Test 56', 'en', '2019-02-03 16:15:02', NULL),
+(110, 56, 'Vn Test 56', 'Vn Description Test 56', 'vn', '2019-02-03 16:15:02', NULL);
 
 -- --------------------------------------------------------
 
@@ -550,13 +704,13 @@ ALTER TABLE `event_category`
 -- AUTO_INCREMENT for table `event_item`
 --
 ALTER TABLE `event_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `event_item_details`
 --
 ALTER TABLE `event_item_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 
 --
 -- AUTO_INCREMENT for table `image_details`
